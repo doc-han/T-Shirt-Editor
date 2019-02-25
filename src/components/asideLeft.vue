a21285b95921810f88b4142f2be2949bbcf9e4b6
doc-han <yahyafarhan48@gmail.com>
2019-02-25 23:45:15 +0000

    colors added

---
+++ src/components/asideLeft.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <div class="content">
+
+    <h3>TeeSwerg</h3>
+
+    <div class="input">
+      <div class="label">
+        Design Title
+      </div>
+      <input type="text" class="input-control">
+    </div>
+
+    <div class="input">
+      <div class="label">
+        Select colors
+      </div>
+      <div class="colors">
+        <div class="color-item" v-for='(color) in config.colors' :style="'background-color: '+color"></div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['config']
+}
+</script>
+
+<style lang="scss" scoped>
+$color-item-size: 30px;
+  .colors {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  .color-item {
+    width: $color-item-size;
+    height: $color-item-size;
+    border-radius: 50%;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    cursor: pointer;
+  }
+
+</style>
