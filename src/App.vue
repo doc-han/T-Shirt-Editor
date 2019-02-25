116620a5baf1c27ea504a2164d3e92f3f6fd13fe
doc-han <yahyafarhan48@gmail.com>
2019-02-25 23:44:30 +0000

    Page created with link to 3 sub-components

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,115 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="asideLeft">
+    <asideLeft :config="config.left" ></asideLeft>
+    </div>
+    <div class="editor">
+      <editor />
+    </div>
+    <div class="asideRight">
+      <asideRight />
+    </div>
   </div>
 </template>
 
 <script>
+import asideLeft from './components/asideLeft'
+import editor from './components/editor'
+import asideRight from './components/asideRight'
+
 export default {
   name: 'app',
+  components: {
+    asideLeft,
+    editor,
+    asideRight,
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      config: {
+        left: {
+          colors: ['#f44336','#009688','#ff9800','#795548','#607d8b'],
+        }
+      }
     }
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+$aside-width: 300px;
+$editor-width: calc(80vw - 600px); // The value subtracted in here is ($aside-width * 2)
+$window-heigth: 90vh;
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: $window-heigth;
+  margin-top: 5vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 }
+
+@mixin borderStyle(){
+  border: 1px solid #e8e8e8;
+  border-radius: 5px;
+  background-color: #fbfbfb;
+}
+
+.asideLeft {
+  flex-basis: $aside-width;
+  @include borderStyle;
+}
+
+.editor {
+  flex-basis: $editor-width;
+}
+
+.asideRight {
+  flex-basis: $aside-width;
+  @include borderStyle;
+}
+
+.content {
+  width: $aside-width;
+}
+
+.input {
+
+  margin-top: 10px;
+
+  .label {
+      margin-left: 10px;
+      text-align: left;
+  }
+  .input-control {
+    width: $aside-width - 40px;
+    height: 30px;
+    padding: 0 10px;
+    outline: none;
+    font-size: 20px;
+    border-radius: 5px;
+    border: 1px solid #e8e8e8;
+  }
+
+  .colors {
+    width: $aside-width - 40px;
+    border: 1px solid #e8e8e8;
+    margin: auto;
+    padding: 10px 10px 0 10px;
+  }
+}
+
+
 
 h1, h2 {
   font-weight: normal;
