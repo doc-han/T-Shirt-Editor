12da72192c3329ef56da470e91b63d10b24f4391
doc-han <yahyafarhan48@gmail.com>
2019-02-25 23:45:47 +0000

    Editor created

---
+++ src/components/editor.vue
@@ -0,0 +1,74 @@
+<template lang="html">
+  <div>
+    <div class="bar">
+      <div>
+        <i class="fa fa-trash"></i>
+      </div>
+      <div>
+        <i class="fa fa-image"></i>
+      </div>
+      <div>
+        <i class="fa fa-broom"></i>
+      </div>
+    </div>
+    <div class="canvas_bg">
+      <div class="image_bg">
+        <img src="../assets/crew_front.png" alt="">
+        <div id="drawing_area">
+          <canvas id="canvas" width="300" height="450"></canvas>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.canvas_bg {
+    display: inline-block;
+  }
+  .image_bg {
+    width: 460px;
+    height: 575px;
+  }
+  .image_bg {
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+  #drawing_area {
+    position: absolute;
+    left: 110px;
+    top: 100px;
+    width: 300px;
+    height: 450px;
+  }
+  #drawing_area:hover {
+    border: 1px solid #343434;
+    cursor: pointer;
+  }
+  .bar {
+    height: 40px;
+    width: 100%;
+    background-color: #fbfbfb;
+    border: 1px solid #e8e8e8;
+    margin: 3px auto;
+    display: flex;
+    flex-direction: row;
+  }
+  .bar div {
+    height: 30px;
+    line-height: 30px;
+    padding: 0 10px;
+    font-weight: bold;
+    color: #343434;
+    border: 1px solid #e8e8e8;
+    border-radius: 5px;
+    margin: 5px 2px;
+    cursor: pointer;
+  }
+</style>
