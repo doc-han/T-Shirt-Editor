4f5d6d3ee65f8ddf5b78014fce3e931e3378fa25
doc-han <yahyafarhan48@gmail.com>
2019-03-05 21:18:13 +0000

    config back to production

--- webpack.config.js
+++ webpack.config.js
@@ -86,13 +86,13 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"development"',
+        NODE_ENV: '"production"',
         CLOUD_NAME: '"farhano123"',
         API_KEY: '"817999527351872"',
         UPLOAD_PRESET: '"nak6ious"',
